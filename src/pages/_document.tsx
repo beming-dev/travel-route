15bd802558def080ccaf43344dd90465dc8a6695
beming-dev <mingfordev@gmail.com>
2023-03-29 19:36:48 +0900

    Initialization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-                <Script
-                    src="//dapi.kakao.com/v2/maps/sdk.js?appkey=a77bcc55c10ff3695437d27f1c29fed8&libraries=services,clusterer&autoload=false"
-                    strategy="beforeInteractive"
-                />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
